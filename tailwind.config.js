13e76a48a3bfbfc71976bc026d5e96851a8e9863
alphanumericaly <github@sfjmail.com>
2026-02-25 17:02:32 -0500

    Fix Tailwind v4 configuration and restore missing config files

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./hugo_stats.json", "./layouts/**/*.html", "./content/**/*.{html,md}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
